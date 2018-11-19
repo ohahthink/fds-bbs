e84a86569a10d48556c154bea8e2a3016fc0ec61
ohahthink <ohahthink@gmail.com>
2018-11-19 16:44:06 +0900

    게시물 목록 출력 기능

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import logo from './logo.svg';
 import './App.css';
 import LoginForm from './components/LoginForm'
 import RegisterForm from './components/RegisterForm'
-
+import PostList from './components/PostList'
 
 // 로그인 폼에 회원가입 버튼 만들기
 // 회원가입 버튼 클릭하면 회원가입 폼 보여주기
@@ -19,8 +19,9 @@ class App extends Component {
     super(props);
     // page === 'login' -> 로그인 페이지
     // page === 'register' -> 회원가입 페이지
+    // page === 'post-list' -> 게시물 목록 페이지
     this.state = {
-      page: 'login'
+      page: 'post-list'
     }
   }
 
@@ -37,6 +38,8 @@ class App extends Component {
           <LoginForm onRegister={() => this.handleRegisterPage()} />
         ) : this.state.page === 'register' ? (  
           <RegisterForm />
+        ) : this.state.page === 'post-list' ? (
+          <PostList/>
         ) : null}
       </div>
     );
