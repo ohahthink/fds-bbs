437de4894fc4c1522e454cee174b25ade2c0acde
ohahthink <ohahthink@gmail.com>
2018-11-19 17:06:05 +0900

    ref사용

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,10 +2,21 @@ import React from 'react'
 import api from '../api'
 
 export default class LoginForm extends React.Component {
+    // ref는 DOM노드를 가리키는 화살표
+    // current는 가리키던 DOM노드를 가져옵니다
+    constructor(props) {
+      super(props)
+        // DOM노드를 가리키는 화살표
+      this.usernameRef = React.createRef()
+      this.passwordRef = React.createRef()
+    }
+    
     
     async handleSubmit(e) {
         e.preventDefault()
-        const username = e.target.elements.username.value
+        // username은 current를 사용한 예제
+        // form을 안쓰고 제어되지 않는 컴포넌트를 가져오고 싶을때는 ref를 사용해야합니다, 폼을 쓰지 말아야할 때도 있기 때문입니다
+        const username = this.usernameRef.current.value
         const password = e.target.elements.password.value
         // FIXME: 사용자 이름 중복체크 해야함
         // api설정 해줘야 합니다
@@ -24,8 +35,8 @@ export default class LoginForm extends React.Component {
         <React.Fragment>
             <form onSubmit={e => this.handleSubmit(e)}>
               <h1>로그인</h1>
-              <input type="text" name="username" />
-              <input type="text" name="password" />
+              <input ref={this.usernameRef} type="text" name="username" />
+              <input ref={this.passwordRef} type="text" name="password" />
               <button>로그인</button>
             </form>
             <button onClick={() => onRegister()}>회원 가입</button>
